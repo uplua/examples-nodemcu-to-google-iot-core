c2f834abf509835cd90d1291bc9b729d859ead72
Gaurav Kumar <gauravkumarbtp@gmail.com>
2019-07-25 14:33:59 +0530

    Create init.lua

---
+++ working_example1/init.lua
@@ -0,0 +1,15 @@
+mytimer = tmr.create()
+station_cfg={}
+station_cfg.ssid="phab"  -- Enter SSID here
+station_cfg.pwd="hukka123"  -- Enter password here
+
+
+wifi.setmode(wifi.STATION)  -- set wi-fi mode to station
+wifi.sta.config(station_cfg)-- set ssid&pwd to config
+wifi.sta.connect(1)         -- connect to router
+
+ip = wifi.sta.getip()       -- IP address of the connected access point 
+
+print(ip)
+
+mytimer:alarm(60000, tmr.ALARM_AUTO, function() dofile("speak.lua") end)
